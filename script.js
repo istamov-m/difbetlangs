9332db67e965f131631ed79ba3a85901a5f58eab
muboriz <121181983+istamov-m@users.noreply.github.com>
2024-03-04 15:30:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.querySelector(".scroll-down").addEventListener("click", function(){
+    document.querySelector(".scroll-down").classList.add("hide")
+})
+
+document.querySelector(".c-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
+
+document.querySelector(".cpp-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
+
+document.querySelector(".cs-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
+
+document.querySelector(".java-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
+
+document.querySelector(".js-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
+
+document.querySelector(".php-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
+
+document.querySelector(".kt-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
+
+document.querySelector(".rust-bg").addEventListener("mouseover", function(){
+    document.querySelector(".scroll-down").classList.remove("hide")
+})
